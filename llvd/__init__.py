bc86fa4f2f7b57d61452408bae08966216413dcb
Volker <vmg@arachnion.de>
2021-01-04 18:51:25 +0100

    Added the possibility to save login data in an INI file ~/.llvd

--- llvd/__init__.py
+++ llvd/__init__.py
@@ -5,9 +5,15 @@ import sys
 import re
 from llvd.app import App
 from llvd import config
+from pathlib import Path
+import os
+from os import access, R_OK
+from os.path import isfile
+import configparser
 
 BOLD = "\033[1m"  # Makes the text bold
 RED_COLOR = "\u001b[31m"  # Makes the text red
+CONF_FILE_NAME = ".llvd"
 
 
 @click.command()
@@ -20,7 +26,24 @@ def main(course, resolution):
     Linkedin learning video downloader cli tool
 
     example: llvd --course "Java 8 Essential"
+
+    Login credentials can be put in ~/.llvd (optional!)
+
+    Example:
+
+        [login]
+
+        email=<EMAIL>
+
+        password=<PASSWORD>
     """
+    conf_file = str(Path.home()) + os.path.sep + CONF_FILE_NAME
+    if isfile(conf_file) and access(conf_file, R_OK):
+        config = configparser.ConfigParser()
+        config.read(conf_file)
+        email = config.get('login', 'email', fallback='')
+        password = config.get('login', 'password', fallback='')
+
     if len(sys.argv) == 1:
         click.echo(f"{RED_COLOR}{BOLD}Missing required arguments: llvd --help")
         sys.exit(0)
@@ -30,8 +53,10 @@ def main(course, resolution):
     course_slug = course.replace(" ", "-").replace(":-", "-").replace(
         "-&", ""
     ).replace(".", "-")
-    email = click.prompt("Please enter your Linkedin email address")
-    password = click.prompt("Enter your Linkedin Password", hide_input=True)
+    if len(email)==0:
+        email = click.prompt("Please enter your Linkedin email address")
+    if len(password)==0:
+        password = click.prompt("Enter your Linkedin Password", hide_input=True)
 
     llvd = App(email, password, course_slug, resolution)
     llvd.run()
